d63b938d1a985fa94d124886dc2c0d8f61b10186
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2023-06-25 12:29:54 +0200

    Add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text">
+  <p>
+    Brawo, brawo... Nie ma to jak rozwiązywanie zagadki po wieczorze chlania i
+    grania w gry, prawda?
+  </p>
+  <p>
+    Ale jak wiadomo, wszystko co dobre, wymaga czasu. Więc na kolejną część
+    zagadki musisz poczekać.
+  </p>
+</div>
+<div class="picture">
+  <img src="https://i.ibb.co/dMGfVZZ/tree1-1.png" alt="tree1-1" />
+</div>
+
+<style>
+  :global(body) {
+    background-image: url("https://i.ibb.co/vBRFT6T/bg-2-1.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  .text {
+    margin: 250px auto 0 auto;
+    text-align: center;
+    max-width: 600px;
+    font-family: "Caveat", cursive;
+    font-size: 30px;
+    text-shadow: 5px 3px 7px rgba(66, 68, 90, 1);
+  }
+
+  .picture {
+    text-align: center;
+  }
+</style>
