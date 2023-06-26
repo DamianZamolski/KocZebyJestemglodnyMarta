8699fe8d4639b722be67e7514609b977603bb7d5
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2023-06-26 14:33:12 +0200

    Add conditional rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,32 @@
-<div class="text">
-  <p>
-    Brawo, brawo... Nie ma to jak rozwiązywanie zagadki po wieczorze chlania i
-    grania w gry, prawda?
-  </p>
-  <p>
-    Ale jak wiadomo, wszystko co dobre, wymaga czasu. Więc na kolejną część
-    zagadki musisz poczekać.
-  </p>
-</div>
-<div class="picture">
-  <img src="https://i.ibb.co/dMGfVZZ/tree1-1.png" alt="tree1-1" />
-</div>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let visitCount = 0;
+  onMount(() => {
+    visitCount = parseInt(localStorage.getItem("visitCount") || "0") + 1;
+    localStorage.setItem("visitCount", visitCount.toString());
+  });
+</script>
+
+{#if visitCount === 1}
+  <div class="text">
+    <p>
+      Brawo, brawo... Nie ma to jak rozwiązywanie zagadki po wieczorze chlania i
+      grania w gry, prawda?
+    </p>
+    <p>
+      Ale jak wiadomo, wszystko co dobre, wymaga czasu. Więc na kolejną część
+      zagadki musisz poczekać.
+    </p>
+    <p>
+      I nie myśl, że dasz radę mnie oszukać - o drzewko trzeba regularnie dbać.
+      Albo będziesz je podlewać, albo nie uda Ci się zdobyć nagrody!
+    </p>
+  </div>
+  <div class="picture">
+    <img src="https://i.ibb.co/dMGfVZZ/tree1-1.png" alt="tree1-1" />
+  </div>
+{/if}
 
 <style>
   :global(body) {
