ab9cd7f9127e82f8f798004e5309e53bf3bf012f
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2023-06-30 14:29:55 +0200

    Add third quest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,11 @@
   let timer: number | null = null;
   let podlano = 0;
   let success = false;
+  let thirdQuest = false;
 
   onMount(() => {
     success = JSON.parse(localStorage.getItem("success") || "false");
+    thirdQuest = JSON.parse(localStorage.getItem("third-quest") || "false");
   });
 
   function podlej() {
@@ -23,15 +25,42 @@
 
     podlano++;
   }
+
+  let jablka = 0;
+  let gruszki = 0;
+
+  function jablko() {
+    gruszki = 0;
+    thirdQuest = ++jablka === 5;
+    localStorage.setItem("third-quest", JSON.stringify(thirdQuest));
+  }
+
+  function gruszka() {
+    jablka = 0;
+    thirdQuest = ++gruszki === 5;
+    localStorage.setItem("third-quest", JSON.stringify(thirdQuest));
+  }
 </script>
 
-<img src="https://i.ibb.co/CVBFzZJ/tree2-1.png" alt="tree2-1" />
-{#if success}
+{#if thirdQuest}
+  <img
+    src="https://i.ibb.co/y6SgnxF/tree3-2-removebg-preview.png"
+    alt="tree3-2-removebg-preview"
+  />
+  <p>Świetnie. Niech i tak będzie. Kiedyś.</p>
+{:else if success}
+  <img
+    src="https://i.ibb.co/y6SgnxF/tree3-2-removebg-preview.png"
+    alt="tree3-2-removebg-preview"
+  />
   <p>
-    No dobrze, już wystarczająco nakapałeś. Teraz daj mi chwilę ochłonąć po tym
-    złotym deszczu.
+    Jeszcze trochę i będziesz mógł zbierać owoce swojej pracy. Rośnie na mnie
+    pięć POTĘŻNYCH KONARÓW. Myślisz, żę urodzę jabłka czy gruszki?
   </p>
+  <button on:click={jablko}>Jablko</button>
+  <button on:click={gruszka}>Gruszka</button>
 {:else}
+  <img src="https://i.ibb.co/CVBFzZJ/tree2-1.png" alt="tree2-1" />
   <p>Osiemnaście listków? Mam nadzieję, że umiesz dbać o swoje drzewko.</p>
   <button on:click={podlej}>Podlej</button>
 {/if}
